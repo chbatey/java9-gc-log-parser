a9951865f076c25ecfa658564a8e508d80e3daf7
Maciej Makowski <maciejm.github@cfiet.net>
2017-07-29 19:02:39 +0100

    Reintroduced HTTP endpoint

--- src/main/scala/info/batey/GcService.scala
+++ src/main/scala/info/batey/GcService.scala
@@ -2,31 +2,39 @@ package info.batey
 
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.event.{Logging, LoggingAdapter}
+import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Keep, Sink}
 import info.batey.GCLogFileModel.TimeOffset
 import info.batey.actors.GcStateActor.{GcState, GenerationSizes, HeapSize}
-import info.batey.actors.{GcStateActor, UnknownLineEvent, PauseActor}
+import info.batey.actors.{GcStateActor, PauseActor, UnknownLineEvent}
+
 import scala.concurrent.ExecutionContext.Implicits._
 import spray.json._
 
-object GcService extends GcLogStream with GcStateJson {
+import scala.io.StdIn
 
-  override implicit val system: ActorSystem = ActorSystem("GCParser")
-  override implicit val materialiser: ActorMaterializer = ActorMaterializer()
-
-  override val log: LoggingAdapter = Logging(system, "main")
-
-  override val young: ActorRef = system.actorOf(Props(classOf[PauseActor]), "YoungGen")
-  override val unknown: ActorRef = system.actorOf(Props(classOf[UnknownLineEvent]), "UnknownMsgs")
-  override val gcState: ActorRef = system.actorOf(Props(classOf[GcStateActor]), "GcState")
+object GcService extends GcStateJson {
+  implicit val system: ActorSystem = ActorSystem("GCParser")
+  implicit val materialiser: ActorMaterializer = ActorMaterializer()
+  implicit val log: LoggingAdapter = Logging(system, "main")
 
   def main(args: Array[String]): Unit = {
-    fromGcLog
-      .map(_.toJson)
-      .toMat(Sink.foreach(println))(Keep.right)
-      .run()
-      .flatMap(_ => system.terminate())
+    implicit val logStream = GcLogStream.create()
+
+    val host = "localhost"
+    val port = 9090
+
+    val httpBinding = for {
+      httpBinding <- Http().bindAndHandle(HttpFrontEnd.routes, host, port)
+      consoleRun <- logStream.fromGcLog
+        .map(_.toJson)
+        .toMat(Sink.foreach(println))(Keep.right)
+        .run()
+    } yield { httpBinding }
+
+    println(s"Servig HTTP requests at http://${host}:${port}/stream/gc")
+    println("Press ENTER to terminate")
+    StdIn.readLine()
   }
 }
-
